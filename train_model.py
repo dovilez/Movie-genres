455c2de8e97168d410326e8d393f3504338d0478
Žaltauskaitė Dovilė <dovzal1@ktu.lt>
2021-07-03 13:42:29 +0300

    formatting

--- train_model.py
+++ train_model.py
@@ -11,21 +11,19 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.metrics import f1_score
 import scraping.scraping as scraping
 
-def main() -> None:
-    # Code to scrape the imdb website if it hasn't been done yet
+def main():
     # search_words = ['action', 'comedy', 'sci-fi', 'horror', 'romance', 'thriller', 'drama', 'mystery', 'crime',
     #                'animation', 'adventure', 'fantasy', 'superhero']
     # df = scraping.collect_keywords(search_words, 100)
     # df.to_csv("imdb.csv")
-    
-    # read the scraped data and train the model
+
     df = pd.read_csv("imdb.csv").dropna()
     df = prepare_dataframe(df)
     train_model(df)
     print(df)
 
 
-def prepare_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+def prepare_dataframe(df):
     """
     Function to prepare the dataframe
     :param df: DataFrame to prepare
@@ -38,7 +36,7 @@ def prepare_dataframe(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def clean_text(text: str) ->  str:
+def clean_text(text):
     """
     Function for text cleaning
     :param text: text to clean
@@ -51,7 +49,7 @@ def clean_text(text: str) ->  str:
     return text
 
 
-def remove_stopwords(text: str) ->  str:
+def remove_stopwords(text):
     """
     Function to remove stopwords
     :param text: text to clean
@@ -63,15 +61,14 @@ def remove_stopwords(text: str) ->  str:
     return " ".join(no_stopword_text)
 
 
-def train_model(df: pd.DataFrame) -> pd.DataFrame:
+def train_model(df):
     """
     Function to train the model
     :param df: DataFrame for training
     """
     multilabel_binarizer = MultiLabelBinarizer()
     multilabel_binarizer.fit(df["genre"])
-    with open("model/binarizer.pkl", "wb") as output_file:
-        pickle.dump(multilabel_binarizer, output_file)
+    pickle.dump(multilabel_binarizer, open("model/binarizer.pkl", "wb"))
 
     y = multilabel_binarizer.transform(df["genre"])
 
@@ -82,15 +79,14 @@ def train_model(df: pd.DataFrame) -> pd.DataFrame:
     # create TF-IDF features
     tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=10000)
     xtrain_tfidf = tfidf_vectorizer.fit_transform(xtrain)
-    with open("model/vectorizer.pkl", "wb") as output_file:
-        pickle.dump(tfidf_vectorizer, output_file)
+    pickle.dump(tfidf_vectorizer, open("model/vectorizer.pkl", "wb"))
+    #xval_tfidf = tfidf_vectorizer.transform(xval)
 
     lr = LogisticRegression()
     clf = OneVsRestClassifier(lr)
 
     clf.fit(xtrain_tfidf, ytrain)
-    with open("model/classifier.pkl", "wb") as output_file:
-        pickle.dump(clf, output_file)
+    pickle.dump(clf, open("model/classifier.pkl", "wb"))
 
 
 if __name__ == "__main__":
